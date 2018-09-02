116de723bd8d81aacd40008e812cc190445c4cd3
sergiu mihaltan <sergiurmihaltan@gmail.com>
2018-09-01 22:47:33 -0400

    1

---
+++ robot/code/recepteur/recepteur.ino
@@ -0,0 +1,76 @@
+#include "Musique.h"
+#include "Moteur.h"
+#include "NewPing.h"
+#include <VirtualWire.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     13  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 400
+
+Moteur moteur;
+Musique musique;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+int RF_RX_PIN = A0;
+
+
+int distance(){
+  
+  delay(100);  // Wait 500ms between pings (about 2 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  int distance = uS / US_ROUNDTRIP_CM;
+  return distance;
+  
+  }
+
+void setup(){
+ vw_set_rx_pin(RF_RX_PIN);  // Setup receive pin.
+ vw_setup(2000); // Transmission speed in bits per second.
+ vw_rx_start(); // Start the PLL receiver.
+  }
+void loop(){
+ uint8_t buf[VW_MAX_MESSAGE_LEN];
+ uint8_t buflen = VW_MAX_MESSAGE_LEN;
+ 
+   if(vw_get_message(buf, &buflen)){
+    for(int i = 0; i < buflen; ++i) {
+      
+      if(buf[i] == 49){ //avancer gauche
+        
+        moteur.Vitesse(80,0);
+        delay(400);
+        moteur.Vitesse(0,0);
+        
+        }
+     else if(buf[i] == 56 ){ //arreter
+   
+        moteur.Vitesse(0,0);
+        
+        }
+     else if(buf[i] == 51){ //avancer droite
+  
+        moteur.Vitesse(0,80);
+        delay(400);
+        moteur.Vitesse(0,0);
+        
+        }
+    
+     else if(buf[i] == 53){ //avancer
+      
+         moteur.Vitesse(80,75);
+        delay(400);
+        moteur.Vitesse(0,0);
+        
+        }
+      
+     else if(buf[i] == 55){ //musique
+      moteur.Vitesse(0,0);
+       musique.zelda();
+        
+       }
+  
+    }
+    
+   }
+}
+
---
+++ robot/code/transmetteur/transmetteur.ino
@@ -0,0 +1,66 @@
+#include <VirtualWire.h>
+int RF_TX_PIN = A1;
+
+/*Serial.print(data5);
+Serial.print(" ");
+Serial.print(data4);
+Serial.print(" ");
+Serial.print(data3);
+Serial.print(" ");
+Serial.print(data2);
+Serial.println(" ");*/ //debug
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(A5,INPUT);
+pinMode(A4,INPUT);
+pinMode(A3,INPUT);
+pinMode(A2,INPUT);
+
+vw_set_tx_pin(RF_TX_PIN); // Setup transmit pin
+vw_setup(2000); // Transmission speed in bits per second.
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int data5 = analogRead(A5);
+int data4 = analogRead(A4);
+int data3 = analogRead(A3);
+int data2 = analogRead(A2);
+
+if(data5 <= 30){ //avancer gauche
+  const char *msg = "1";
+  vw_send((uint8_t *)msg,1); 
+  vw_wait_tx();
+}
+
+if(data4 < 20){ //avancer droite
+  const char *msg = "3";
+  vw_send((uint8_t *)msg, 1); 
+  vw_wait_tx();
+}
+
+if(data3 < 20 && data2 < 20 ){ //avancer
+  const char *msg = "5";
+  vw_send((uint8_t *)msg,1); 
+  vw_wait_tx();
+}
+
+if(data5 <= 30 && data4 < 20 && data3 < 20 && data2 < 20){ //musique
+  const char *msg = "7";
+  vw_send((uint8_t *)msg, 1); 
+  vw_wait_tx();
+}
+
+if((data3 >= 20 && data2 >= 20) || (data5 > 30 && data4 > 20)){ //arreter
+  const char *msg = "8";
+  vw_send((uint8_t *)msg, 1); 
+  vw_wait_tx();
+  
+}
+
+}
